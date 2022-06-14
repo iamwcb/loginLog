6d18fb98a02b8da16d772fc20a94ebe7ef76baf1
ilucaswu <ilucaswu@163.com>
2022-06-14 16:33:49 +0800

    base version

--- main.py
+++ main.py
@@ -1,38 +1,61 @@
 import re
 import sys
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, qApp, QTableWidgetItem, QFileDialog, QHeaderView
 
 from mainWindow import Ui_MainWindow
+from ui_functions import *
+import resources_rc
 
 
 class LoginLog(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(LoginLog, self).__init__(parent)
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.load_log)
+        self.openBtn.clicked.connect(self.load_log)
         # 列宽自动分配
         self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        UIFunctions.uiDefinitions(self)
+
+    def mousePressEvent(self, event):  # 以下3个函数用来使窗口可以拖动
+        if event.button() == Qt.LeftButton:
+            self.m_drag = True
+            self.m_DragPosition = event.globalPos() - self.pos()
+            event.accept()
+
+    def mouseMoveEvent(self, QMouseEvent):
+        if QMouseEvent.buttons() and Qt.LeftButton:
+            self.move(QMouseEvent.globalPos() - self.m_DragPosition)
+            QMouseEvent.accept()
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_drag = False
 
     def load_log(self):
         openfile_name, ok = QFileDialog.getOpenFileName(self, '选择文件', '', 'log file(*.log)')
         if ok:
+            self.tableWidget.clear()
+            self.tableWidget.setRowCount(0)
             fo = open(openfile_name, "rb")  # 一定要用'rb'因为seek 是以bytes来计算的
-            index = 0
+            rowcount = 0
             for line in fo.readlines():
-                mat1 = re.search(r"(\d{4}[-/]\d{1,2}[-/]\d{1,2})", str(line.decode()))
-                mat2 = re.search(r"(\d{2}:\d{2}:\d{2},\d{3})", str(line.decode()))
-                mat3 = re.search('assigned to (.*)~', str(line.decode()))
+                # rowcount = self.tableWidget.rowCount()
+                mat1 = re.search(r"(\d{4}[-/]\d{1,2}[-/]\d{1,2})", str(line.decode("gbk")))
+                mat2 = re.search(r"(\d{2}:\d{2}:\d{2},\d{3})", str(line.decode("gbk")))
+                mat3 = re.search('assigned to (.*)~', str(line.decode("gbk")))
                 if mat1 and mat2 and mat3:
-                    self.tableWidget.insertRow(index)
+                    self.tableWidget.insertRow(rowcount)
                     item1 = QTableWidgetItem(mat1.group(0))
                     item2 = QTableWidgetItem(mat2.group(0))
                     item3 = QTableWidgetItem(mat3.group(1))
-                    self.tableWidget.setItem(index, 0, item3)
-                    self.tableWidget.setItem(index, 1, item1)
-                    self.tableWidget.setItem(index, 2, item2)
-                    index = index + 1
-
+                    item1.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+                    item2.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+                    item3.setTextAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+                    self.tableWidget.setItem(rowcount, 0, item3)
+                    self.tableWidget.setItem(rowcount, 1, item1)
+                    self.tableWidget.setItem(rowcount, 2, item2)
+                    rowcount = rowcount + 1
             fo.close()
 
 
--- mainWindow.py
+++ mainWindow.py
@@ -14,11 +14,145 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1020, 657)
+        MainWindow.resize(880, 1021)
+        MainWindow.setMinimumSize(QtCore.QSize(880, 600))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
-        self.tableWidget.setGeometry(QtCore.QRect(10, 50, 1001, 591))
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout_3.setContentsMargins(10, 10, 10, 10)
+        self.horizontalLayout_3.setSpacing(0)
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.drop_shadow_frame = QtWidgets.QFrame(self.centralwidget)
+        self.drop_shadow_frame.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(42, 44, 111, 255), stop:0.521368 rgba(28, 29, 73, 255));\n"
+"border-radius: 10px;")
+        self.drop_shadow_frame.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.drop_shadow_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.drop_shadow_frame.setObjectName("drop_shadow_frame")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.drop_shadow_frame)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.title_bar = QtWidgets.QFrame(self.drop_shadow_frame)
+        self.title_bar.setMaximumSize(QtCore.QSize(16777215, 50))
+        self.title_bar.setStyleSheet("background-color: none;")
+        self.title_bar.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.title_bar.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.title_bar.setObjectName("title_bar")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.title_bar)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.frame_title = QtWidgets.QFrame(self.title_bar)
+        self.frame_title.setMinimumSize(QtCore.QSize(0, 50))
+        font = QtGui.QFont()
+        font.setFamily("Roboto Condensed Light")
+        font.setPointSize(14)
+        self.frame_title.setFont(font)
+        self.frame_title.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_title.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_title.setObjectName("frame_title")
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout(self.frame_title)
+        self.horizontalLayout_5.setContentsMargins(15, 0, 0, 0)
+        self.horizontalLayout_5.setSpacing(0)
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.label_title = QtWidgets.QLabel(self.frame_title)
+        font = QtGui.QFont()
+        font.setFamily("Consolas")
+        font.setPointSize(14)
+        self.label_title.setFont(font)
+        self.label_title.setStyleSheet("color: rgb(60, 231, 195);")
+        self.label_title.setObjectName("label_title")
+        self.horizontalLayout_5.addWidget(self.label_title)
+        self.openBtn = QtWidgets.QPushButton(self.frame_title)
+        self.openBtn.setStyleSheet("QPushButton{\n"
+"color: white;\n"
+"font-family: consolas;\n"
+"font : 16px;}\n"
+"")
+        self.openBtn.setObjectName("openBtn")
+        self.horizontalLayout_5.addWidget(self.openBtn)
+        self.horizontalLayout.addWidget(self.frame_title)
+        self.frame_btns = QtWidgets.QFrame(self.title_bar)
+        self.frame_btns.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.frame_btns.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_btns.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_btns.setObjectName("frame_btns")
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout(self.frame_btns)
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.btn_maximize = QtWidgets.QPushButton(self.frame_btns)
+        self.btn_maximize.setMinimumSize(QtCore.QSize(16, 16))
+        self.btn_maximize.setMaximumSize(QtCore.QSize(17, 17))
+        self.btn_maximize.setStyleSheet("QPushButton {\n"
+"    border: none;\n"
+"    border-radius: 8px;    \n"
+"    background-color: rgb(85, 255, 127);\n"
+"}\n"
+"QPushButton:hover {    \n"
+"    background-color: rgba(85, 255, 127, 150);\n"
+"}")
+        self.btn_maximize.setText("")
+        self.btn_maximize.setObjectName("btn_maximize")
+        self.horizontalLayout_4.addWidget(self.btn_maximize)
+        self.btn_minimize = QtWidgets.QPushButton(self.frame_btns)
+        self.btn_minimize.setMinimumSize(QtCore.QSize(16, 16))
+        self.btn_minimize.setMaximumSize(QtCore.QSize(17, 17))
+        self.btn_minimize.setStyleSheet("QPushButton {\n"
+"    border: none;\n"
+"    border-radius: 8px;        \n"
+"    background-color: rgb(255, 170, 0);\n"
+"}\n"
+"QPushButton:hover {    \n"
+"    background-color: rgba(255, 170, 0, 150);\n"
+"}")
+        self.btn_minimize.setText("")
+        self.btn_minimize.setObjectName("btn_minimize")
+        self.horizontalLayout_4.addWidget(self.btn_minimize)
+        self.btn_close = QtWidgets.QPushButton(self.frame_btns)
+        self.btn_close.setMinimumSize(QtCore.QSize(16, 16))
+        self.btn_close.setMaximumSize(QtCore.QSize(17, 17))
+        self.btn_close.setStyleSheet("QPushButton {\n"
+"    border: none;\n"
+"    border-radius: 8px;        \n"
+"    background-color: rgb(255, 0, 0);\n"
+"}\n"
+"QPushButton:hover {        \n"
+"    background-color: rgba(255, 0, 0, 150);\n"
+"}")
+        self.btn_close.setText("")
+        self.btn_close.setObjectName("btn_close")
+        self.horizontalLayout_4.addWidget(self.btn_close)
+        self.horizontalLayout.addWidget(self.frame_btns)
+        self.verticalLayout.addWidget(self.title_bar)
+        self.content_bar = QtWidgets.QFrame(self.drop_shadow_frame)
+        self.content_bar.setStyleSheet("background-color: none;")
+        self.content_bar.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.content_bar.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.content_bar.setObjectName("content_bar")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.content_bar)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.tableWidget = QtWidgets.QTableWidget(self.content_bar)
+        self.tableWidget.setStyleSheet("QTableWidget {\n"
+"background-color: transparent;\n"
+"}\n"
+"\n"
+"QHeaderView::section {\n"
+"background-color: transparent;\n"
+"}\n"
+"\n"
+"QHeaderView {\n"
+"background-color: transparent;\n"
+"color: white;\n"
+"font: bold 10pt;\n"
+"}\n"
+"\n"
+"\n"
+"QTableView{\n"
+"border-color : transparent;\n"
+"color: white;\n"
+"gridline-color: transparent;\n"
+"}\n"
+"\n"
+"QTableCornerButton::section {\n"
+"background-color: transparent;\n"
+"}")
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(3)
         self.tableWidget.setRowCount(0)
@@ -28,13 +162,45 @@ class Ui_MainWindow(object):
         self.tableWidget.setHorizontalHeaderItem(1, item)
         item = QtWidgets.QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(2, item)
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 31))
-        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout_2.addWidget(self.tableWidget)
+        self.verticalLayout.addWidget(self.content_bar)
+        self.credits_bar = QtWidgets.QFrame(self.drop_shadow_frame)
+        self.credits_bar.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.credits_bar.setStyleSheet("background-color: rgb(33, 33, 75);")
+        self.credits_bar.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.credits_bar.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.credits_bar.setObjectName("credits_bar")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.credits_bar)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.frame_label_credits = QtWidgets.QFrame(self.credits_bar)
+        self.frame_label_credits.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_label_credits.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_label_credits.setObjectName("frame_label_credits")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.frame_label_credits)
+        self.verticalLayout_3.setContentsMargins(15, 0, 0, 0)
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.label_credits = QtWidgets.QLabel(self.frame_label_credits)
+        font = QtGui.QFont()
+        font.setFamily("Roboto")
+        self.label_credits.setFont(font)
+        self.label_credits.setStyleSheet("color: rgb(128, 102, 168);")
+        self.label_credits.setObjectName("label_credits")
+        self.verticalLayout_3.addWidget(self.label_credits)
+        self.horizontalLayout_2.addWidget(self.frame_label_credits)
+        self.frame_grip = QtWidgets.QFrame(self.credits_bar)
+        self.frame_grip.setMinimumSize(QtCore.QSize(30, 30))
+        self.frame_grip.setMaximumSize(QtCore.QSize(30, 30))
+        self.frame_grip.setStyleSheet("padding: 5px;")
+        self.frame_grip.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame_grip.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame_grip.setObjectName("frame_grip")
+        self.horizontalLayout_2.addWidget(self.frame_grip)
+        self.verticalLayout.addWidget(self.credits_bar)
+        self.horizontalLayout_3.addWidget(self.drop_shadow_frame)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -42,10 +208,16 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_title.setText(_translate("MainWindow", "Teamcenter LoginInfo"))
+        self.openBtn.setText(_translate("MainWindow", "Open log"))
+        self.btn_maximize.setToolTip(_translate("MainWindow", "Maximize"))
+        self.btn_minimize.setToolTip(_translate("MainWindow", "Minimize"))
+        self.btn_close.setToolTip(_translate("MainWindow", "Close"))
         item = self.tableWidget.horizontalHeaderItem(0)
         item.setText(_translate("MainWindow", "UserId"))
         item = self.tableWidget.horizontalHeaderItem(1)
         item.setText(_translate("MainWindow", "Login Date"))
         item = self.tableWidget.horizontalHeaderItem(2)
         item.setText(_translate("MainWindow", "Login Time"))
-        self.pushButton.setText(_translate("MainWindow", "open"))
+        self.label_credits.setText(_translate("MainWindow", "共性中心|应用软件组"))
+import resources_rc
